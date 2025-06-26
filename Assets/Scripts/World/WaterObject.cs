a33e9b8dfbe3765c8e958d3305e8ffc144fa64be
AmteurPotion <1k0j2i4@gmail.com>
2025-06-27 04:05:58 +0900

    ✨ update  독 디버프 방식으로 변경

--- Assets/Scripts/World/WaterObject.cs
+++ Assets/Scripts/World/WaterObject.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using ToB.Entities;
-using ToB.Player;
+using ToB.Entities.Buffs;
 using UnityEngine;
 
 namespace ToB.Worlds
@@ -13,47 +9,30 @@ namespace ToB.Worlds
     [Tooltip("피해를 주는 초 단위 주기입니다.")] public float delay = 1;
     [Tooltip("플레이어에게만 피해를 줄 지 여부입니다.")] public bool onlyPlayer = true;
     
-    private readonly Dictionary<Collider2D, Coroutine> coroutines = new();
-    
     private void OnTriggerEnter2D(Collider2D other)
     {
-      if (other && !coroutines.ContainsKey(other) && other.TryGetComponent<IDamageable>(out var damageable))
+      if (other )
       {
-        coroutines.Add(other, StartCoroutine(Damage(damageable)));
         other.gameObject.SendMessage("WaterEnter", this, SendMessageOptions.DontRequireReceiver);
+      }
+    }
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+      if (other)
+      {
+        if (other.TryGetComponent<BuffController>(out var buffs))
+        {
+          buffs.Apply(Buff.Poison, new BuffInfo(1, 10), true);
+        }
       }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-      if (other && coroutines.TryGetValue(other, out var coroutine))
+      if (other)
       {
-        StopCoroutine(coroutine);
-        coroutines.Remove(other);
         other.gameObject.SendMessage("WaterExit", this, SendMessageOptions.DontRequireReceiver);
-      }
-    }
-
-    private void OnDestroy()
-    {
-      if (coroutines != null)
-      {
-        foreach (var coroutine in coroutines.Values)
-        {
-          StopCoroutine(coroutine);
-        }
-        coroutines.Clear();
-      }
-    }
-
-    private IEnumerator Damage(IDamageable comp)
-    {
-      for (;;)
-      {
-        yield return new WaitForSeconds(delay);
-        
-        if(!onlyPlayer || comp is PlayerCharacter)
-          comp.Damage(damage);
       }
     }
   }
