35677f3a96e21cd9d067cf916ff6af67c6c2c66d
DevPotion <1k0j2i4@gmail.com>
2025-07-02 21:59:00 +0900

    update 레이어 체크 기능 추가

--- Assets/Scripts/Utils/MathUtil.cs
+++ Assets/Scripts/Utils/MathUtil.cs
@@ -123,5 +123,11 @@ namespace ToB.Utils
     }
 
     #endregion
+    
+    #region Layer
+    
+    public static bool Contains(this LayerMask mask, int layer) => (mask.value & (1 << layer)) != 0;
+    
+    #endregion
   }
 }
