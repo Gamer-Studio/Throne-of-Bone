42490d23fcfc1f461dd30c97062c133a109ee21c
DevPotion <1k0j2i4@gmail.com>
2025-07-03 12:05:21 +0900

    🐛 bugfix 빌드시 에러 수정

--- Assets/Scripts/World/Room.cs
+++ Assets/Scripts/World/Room.cs
@@ -12,10 +12,12 @@ namespace ToB.Worlds
   public class Room : MonoBehaviour
   {
     [Label("일반 적 소환 정보")] public SerializableDictionary<Transform, GameObject> normalEnemyTable = new();
-    [ContextMenuItem("내부 링크 찾기", nameof(FindLinks))]
 
     #region Binding
 
+    #if UNITY_EDITOR
+    [ContextMenuItem("내부 링크 찾기", nameof(FindLinks))]
+    #endif
     [Foldout("Binding")] public List<RoomLink> links = new();
     [Foldout("Binding"), SerializeField] private Transform entityContainer; 
     [field: Foldout("Binding"), SerializeField] public RoomBackground Background { get; private set; }
