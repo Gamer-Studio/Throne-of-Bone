9e3fd1c77d247f0b493665aaf895cfac5a94be3f
CloudWanderer <huyn918@gmail.com>
2025-07-07 22:11:04 +0900

    fix. ResourceManager 프리펩 할당 수정
    이게 튜터님은 빌드해도 작동한다 vs GPT는 응 안돼 하는데 일단은 Intro 씬에 UIManager처럼 동일하게 게임오브젝트를 배치한 뒤 인스펙터로 할당하는 방향으로 진행하겠습니다.

---
+++ Assets/Scripts/Entities/Obstacle/Bonfire.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace ToB.Entities.Obstacle
+{
+    public class Bonfire : MonoBehaviour, IInteractable
+    {
+        public bool IsInteractable { get; set; }
+        
+        public void Interact()
+        {
+            
+        }
+
+    }
+}
