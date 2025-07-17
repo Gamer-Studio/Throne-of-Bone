6b964380227e2605f88350da276ba6297808bfcc
DevPotion <1k0j2i4@gmail.com>
2025-07-17 12:09:15 +0900

    update 액션 예외처리

--- Assets/Scripts/Core/InputManager/InputManager_PlayerAction.cs
+++ Assets/Scripts/Core/InputManager/InputManager_PlayerAction.cs
@@ -11,6 +11,7 @@ namespace ToB.Core.InputManager
     {
         public void PlayerMove(InputAction.CallbackContext context)
         {
+            if (!StageManager.Instance) return;
             if (StageManager.Instance.CurrentState != GameState.Play) return;
             player?.Move(context);
         }
@@ -24,24 +25,28 @@ namespace ToB.Core.InputManager
 
         public void PlayerJump(InputAction.CallbackContext context)
         {
+            if (!StageManager.Instance) return;
             if (StageManager.Instance.CurrentState != GameState.Play) return;
             player?.Jump(context);
         }
 
         public void PlayerDash(InputAction.CallbackContext context)
         {
+            if (!StageManager.Instance) return;
             if (StageManager.Instance.CurrentState != GameState.Play) return;
             player?.Dash(context);
         }
 
         public void PlayerRangedAttack(InputAction.CallbackContext context)
         {
+            if (!StageManager.Instance) return;
             if (StageManager.Instance.CurrentState != GameState.Play) return;
             player?.RangedAttack(context);
         }
 
         public void PlayerInteract(InputAction.CallbackContext context)
         {
+            if (!StageManager.Instance) return;
             if (StageManager.Instance.CurrentState != GameState.Play) return;
             
             if(context.performed)
@@ -50,6 +55,7 @@ namespace ToB.Core.InputManager
 
         public void PlayerBlock(InputAction.CallbackContext context)
         {
+            if (!StageManager.Instance) return;
             if (StageManager.Instance.CurrentState != GameState.Play) return;
             player?.Block(context);
         }
