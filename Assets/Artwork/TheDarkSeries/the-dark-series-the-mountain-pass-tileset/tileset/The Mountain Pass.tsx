c3c443f1648afc6944e101604859d6ff744307d9
DevPotion <1k0j2i4@gmail.com>
2025-07-21 19:22:36 +0900

    The Mountain Pass Deco 타일팔레트 추가

---
+++ Assets/Artwork/TheDarkSeries/the-dark-series-the-mountain-pass-tileset/tileset/The Mountain Pass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="The Mountain Pass" tilewidth="64" tileheight="64" tilecount="144" columns="12">
+ <image source="tileset 64x64.png" width="768" height="768"/>
+</tileset>
