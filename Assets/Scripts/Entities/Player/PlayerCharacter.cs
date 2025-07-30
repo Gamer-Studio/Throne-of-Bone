71cab07493522dff6fb3e7686549334888f88812
DevPotion <1k0j2i4@gmail.com>
2025-07-30 15:52:39 +0900

    fix 플레이어 기초 공격력이 공격시마다 바뀌는 문제 수정

--- Assets/Scripts/Entities/Player/PlayerCharacter.cs
+++ Assets/Scripts/Entities/Player/PlayerCharacter.cs
@@ -57,7 +57,7 @@ namespace ToB.Player
     private bool isDamageImmune = false;
     
     // 플레이어 스텟 관리 클래스입니다.
-    [Label("캐릭터 스텟"), Foldout("State")] public PlayerStats stat = new();
+    [Label("캐릭터 스텟")] public PlayerStats stat = new();
 
     // Jump State
     private const string JumpState = nameof(JumpState);
