d05be2c6fc506daf9f4b69369846e672d988c8d6
Lopeca <3167ls3@naver.com>
2025-08-13 15:28:59 +0900

    fix : 배경 위치 조절

---
+++ Assets/Scripts/Utils/SimpleMotion/BGPosHandler.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+namespace ToB
+{
+    public class BGPosHandler : MonoBehaviour
+    {
+        // 기준 좌표는 시뮬레이션속 스테이지에서 수동으로 정합니다
+        private  Vector2 camOriginPos;
+        private Vector2 bgOriginPos;
+
+        private float weight = 0.98f;
+        private void Awake()
+        {
+            camOriginPos = new Vector2(10.26434f, -12.39999f);
+            bgOriginPos = new Vector2(0.6f, -12.6f);
+        }
+
+        private void LateUpdate()
+        {
+            Vector2 posDiff = (Vector2)Camera.main.transform.position - camOriginPos;
+            transform.position = bgOriginPos + posDiff * weight;
+        }
+    }
+}
